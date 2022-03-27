80717307455c115cea751dc4e85a5d8902756945
viniciusym <viniciusyamate5@gmail.com>
2022-03-27 22:51:46 +0100

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+
+describe('testa o componente Not found', () => {
+  it('Teste se página contém um heading h2 com o texto "Page requested not found 😭"',
+    () => {
+      render(<NotFound />);
+
+      const notFoundHeading = screen.getByRole('heading', {
+        name: 'Page requested not found Crying emoji',
+        level: 2,
+      });
+
+      expect(notFoundHeading).toBeInTheDocument();
+    });
+  it('testa se a pagina mostra a imagem especificada', () => {
+    render(<NotFound />);
+
+    const notFoundImageAlt = 'Pikachu crying because the page requested was not found';
+    const notFoundImageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImage = screen.getByAltText(notFoundImageAlt);
+
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage).toHaveAttribute('src', notFoundImageUrl);
+  });
+});
