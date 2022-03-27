fe1b7f814d739371d508cb1bca7b53c2eb470c4b
viniciusym <viniciusyamate5@gmail.com>
2022-03-27 22:17:47 +0100

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('testa componente Favorite Pokemons', () => {
+  it('testa se quando não tiver favoritos uma mensagem é exibida na tela', () => {
+    render(<FavoritePokemons />);
+
+    const notFoundText = screen.getByText('No favorite pokemon found');
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+  it('testa se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(detailsLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    const favoriteButton = screen.getByRole('checkbox');
+    userEvent.click(favoriteButton);
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const { pathname: favoritePath } = history.location;
+    expect(favoritePath).toBe('/favorites');
+
+    const favPokemonimage = screen.getByAltText('Pikachu sprite');
+    expect(favPokemonimage).toBeInTheDocument();
+    expect(favPokemonimage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
